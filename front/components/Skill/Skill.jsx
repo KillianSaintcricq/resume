bfb5073a51486cc990188fd6f23780e20b274083
Killian Saint cricq <killian.40@hotmail.fr>
2016-01-03 16:26:49 +0100

    Like button is now a button and not a link, plus some sass code reorganization

--- front/components/Skill/Skill.jsx
+++ front/components/Skill/Skill.jsx
@@ -5,35 +5,24 @@ import './skill.scss';
 
 export default class Skill extends Component {
 
-    constructor(props) {
-        super(props);
-        this.onVoteClick = this.onVoteClick.bind(this);
-    }
-
-    onVoteClick(e) {
-        e.preventDefault();
-        this.props.onVoteClick();
-    }
-
     render() {
-        const { id, title, votes, tags } = this.props;
+        const { id, title, votes, tags, onVoteClick } = this.props;
         const idS = id.toString();
         return (
             <span className="skill">
-                <a href="#" className="vote hide-on-s-and-lower" onClick={this.onVoteClick} data-tip data-for={idS}>
+                <button className="vote hide-on-s-and-lower" onClick={onVoteClick} data-tip data-for={idS}>
                     <i className="material-icons icon">thumb_up</i>
-                </a>
+                </button>
                 <ReactTooltip id={idS} effect="solid">
                     <span>{votes}</span>
                 </ReactTooltip>
                 <div className="content-container">
                     <h3 className="title">{title}</h3>
                     <div className="skill-tags-container">
-                        {tags.map(
-                            (tag, index) =>
-                                <span className="skill-tags" key={index}>
-                                    {tag}{' '}
-                                </span>
+                        {tags.map((tag, index) =>
+                            <span className="skill-tags" key={index}>
+                                {tag}{' '}
+                            </span>
                         )}
                     </div>
                 </div>
