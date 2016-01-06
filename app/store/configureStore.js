ffc30df9333ff8444e8db95619d75ac779ed4e6c
Killian Saint cricq <killian.40@hotmail.fr>
2016-01-06 19:31:21 +0100

    Remove logging

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -12,7 +12,6 @@ export default function configureStore(initialState) {
     const store = createStoreWithMiddleware(resumeApp, initialState);
 
     if (module.hot) {
-        console.log("wut");
         // Enable Webpack hot module replacement for reducers
         module.hot.accept('../reducers', () => {
             const nextReducer = require('../reducers');
