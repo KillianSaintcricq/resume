5f65c007853014a38f292df21bb77493f2355244
Killian Saint cricq <killian.40@hotmail.fr>
2016-01-07 18:18:50 +0100

    Simplify mongo connection

--- api/index.js
+++ api/index.js
@@ -4,12 +4,7 @@ const skills = require('./services/skills');
 const tags = require('./services/tags');
 
 const router = express.Router();
-
-const options =
-    process.env.MONGO_USER && process.env.MONGO_PASSWORD ?
-    {user: process.env.MONGO_USER, pass: process.env.MONGO_PASSWORD} :
-    {};
-mongoose.connect(`mongodb://${process.env.MONGO_URL}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`, options);
+mongoose.connect(process.env.MONGO_URI);
 
 router.get('/skills', skills.getAll);
 router.get('/tags', tags.getAll);
