e2f544b252848cff5382f132f21796f29dd9f041
Killian Saint cricq <killian.40@hotmail.fr>
2016-01-07 09:24:04 +0100

    Use of ES6 features so far implemented by node to deploy and launch the server

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var apiRoutes = require('./api/routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const apiRoutes = require('./api/routes');
 
-var app = new express();
+const app = new express();
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Front-end configuration /////////////////////////////////////////////////////////////////////////////////////////////
@@ -11,11 +11,11 @@ var app = new express();
 
 // Webpack configuration
 if (process.env.NODE_ENV === 'development') {
-    var webpack = require('webpack');
-    var config = require('./webpack.development.js');
-    var compiler = webpack(config);
-    var webpackDevMiddleware = require('webpack-dev-middleware');
-    var webpackHotMiddleware = require('webpack-hot-middleware');
+    const webpack = require('webpack');
+    const config = require('./webpack.development.js');
+    const compiler = webpack(config);
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
     app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
     app.use(webpackHotMiddleware(compiler));
 }
@@ -35,12 +35,14 @@ app.use('/api', apiRoutes);
 // Serve, server! //////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var port = process.env.PORT || 1993;
+const port = process.env.PORT || 1993;
 
 app.listen(port, function (error) {
     if (error) {
         console.error(error)
     } else {
-        console.info("==> Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+        if (process.env.NODE_ENV === 'development') {
+            console.info("==> Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+        }
     }
 });
