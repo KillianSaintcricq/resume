6fb38d4ee328999baa19bb4e68557749d33358c0
Killian Saint cricq <killian.40@hotmail.fr>
2016-01-07 14:41:58 +0100

    Take into account environment variables (dev/prod) for API

--- api/index.js
+++ api/index.js
@@ -4,8 +4,12 @@ const skills = require('./services/skills');
 const tags = require('./services/tags');
 
 const router = express.Router();
-// TODO: dev vs prod
-mongoose.connect('mongodb://localhost/resume');
+
+const options =
+    process.env.MONGO_USER && process.env.MONGO_PASSWORD ?
+    {user: process.env.MONGO_USER, pass: process.env.MONGO_PASSWORD} :
+    {};
+mongoose.connect(`mongodb://${process.env.MONGO_URL}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`, options);
 
 router.get('/skills', skills.getAll);
 router.get('/tags', tags.getAll);
