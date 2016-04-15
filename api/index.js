d666db0740238e7c3fe2d469650fed46f15af0ef
Killian Saint cricq <killian.40@hotmail.fr>
2016-04-15 13:59:06 +0200

    Delete API for experiences

--- api/index.js
+++ api/index.js
@@ -2,13 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const skills = require('./services/skills');
 const tags = require('./services/tags');
-const experiences = require('./services/experiences');
 
 const router = express.Router();
 mongoose.connect(process.env.MONGO_URI);
 
 router.get('/skills', skills.getAll);
 router.get('/tags', tags.getAll);
-router.get('/experiences', experiences.getAll);
 
 module.exports = router;
